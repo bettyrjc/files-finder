60879198dd88ecefb7681060dc3d8b523ca49a8c
Betty Jimenez <bettyjimenez3010@gmail.com>
2024-10-31 20:54:14 -0300

    :sparkles: add: sort by date and name

--- src/app/files/views/index.tsx
+++ src/app/files/views/index.tsx
@@ -2,39 +2,35 @@
 
 'use client'
 
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import FileItem, { FileItemType } from '../components/FileItem'
-// import { files } from 'src/assets/Data'
 import SearchInput from '../../shared/inputs/SearchInput'
 import Button from '../../shared/buttons/Button'
-import { ArrowDownUp } from 'lucide-react'
+import { ArrowDownUp, Loader } from 'lucide-react'
 import { useKnowledgeBasesService } from '../hooks/useKnowledgeBasesService'
-// import { useFilesListKnowledgeBasesService } from '../hooks/useFilesListKnowledgeBaseService'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { getFilesListKnowledgeBaseService } from '../services/getKnowledgeBase'
 
 const FilesFinder = () => {
   const searchInput = useRef<HTMLInputElement>(null)
+  const [dataValue, setDataValue] = useState<FileItemType[]>([])
+  const [direction, setDirection] = useState('desc');
   const {
     data,
-    isSuccess: isKnowledgeBaseSuccess
+    isSuccess: isKnowledgeBaseSuccess,
+    isLoading: isKnowledgeBaseLoading,
   } = useKnowledgeBasesService()
   const knowledgeBaseInfo = data?.admin[0] || {}
   const knowledgeBaseId = knowledgeBaseInfo?.knowledge_base_id || ''
-
-  // const { data: filesData } = useFilesListKnowledgeBasesService(knowledgeBaseInfo?.knowledge_base_id || '')
   const queryClient = useQueryClient();
 
-  // Query principal para obtener los archivos raíz
   const { data: filesData } = useQuery({
     queryKey: ['knowledge_files', knowledgeBaseId],
     queryFn: () => getFilesListKnowledgeBaseService(knowledgeBaseId),
     enabled: Boolean(knowledgeBaseId) && isKnowledgeBaseSuccess,
   });
 
-  
   const handleDelete = (itemId: string) => {
-    // Función recursiva para encontrar y actualizar elementos
     const removeItem = (items: FileItemType[]): FileItemType[] => {
       return items.filter(item => {
         if (item.inode_id === itemId) {
@@ -47,73 +43,116 @@ const FilesFinder = () => {
       });
     };
 
-    // Actualizar el cache para la consulta principal
     queryClient.setQueryData(
       ['knowledge_files', knowledgeBaseId],
       (oldData: FileItemType[] | undefined) => oldData ? removeItem(oldData) : []
     );
   };
+  const sortDataByDate = (direction = 'desc') => {
+    if (dataValue.length === 0) return;
+    const data = dataValue.sort((a: FileItemType, b: FileItemType) => {
+      const dateA = new Date(a.modified_at).getTime();
+      const dateB = new Date(b.modified_at).getTime();
+      return direction === 'desc'
+        ? dateB - dateA  // Desc (B -> A)
+        : dateA - dateB; // Asc (A -> B)
+    });
 
+    setDirection(direction === 'desc' ? 'asc' : 'desc');
+    setDataValue([...data]);
+  };
+  const sortDataByName = (direction = 'desc') => {
+    if (dataValue.length === 0) return;
+    const data = dataValue.sort((a: FileItemType, b: FileItemType) => {
+      const nameA = a?.inode_path?.path.toLowerCase();
+      const nameB = b?.inode_path?.path.toLowerCase();
+      return direction === 'desc'
+        ? nameB.localeCompare(nameA)  // Desc (B -> A)
+        : nameA.localeCompare(nameB); // Asc (A -> B)
+    });
+
+    setDirection(direction === 'desc' ? 'asc' : 'desc');
+    setDataValue([...data]);
+  }
+
+  useEffect(() => {
+    if (filesData?.length === 0) return
+    setDataValue(filesData)
+  }, [filesData])
   return (
 
     <div className="w-full min-h-[700px] h-full   text-gray-900 mt-0 pt-0">
-      <>
-        {/* header table NTH */}
-        <div className="items-center justify-between gap-6 px-2 py-4 border border-gray-200 md:flex ">
-          <div className='mb-2 md:mb-0'>
-            <Button
-              color='primary'
-              onClick={() => console.log('filter')}
-              size="small"
-              variant="outlined"
-              icon={<ArrowDownUp size={14} />}
-            >
-              Sort
-            </Button>
-          </div>
-          <div >
-            <SearchInput
-              onChange={(e) => console.log(e.target.value)}
-              ref={searchInput}
-            />
-          </div>
-        </div>
-        {/* files */}
-        <div className="mt-2 overflow-x-auto">
-          <table className="min-w-full border divide-y divide-gray-200">
-            <thead className="bg-blue-100">
-              <tr>
-                <th className="w-1/2 px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase border-b">
-                  Name
-                </th>
-                <th className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase border-b">
-                  Date
-                </th>
-                <th className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase border-b">
-                  Kind
-                </th>
-                <th className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase border-b">
-                  Actions
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {filesData?.length > 0 && filesData.map((file: any, index: any) => (
-                <FileItem key={`${file.name}-${index}`} item={file}
-                  level={0}
-                  parentId={file.inode_id}
-                  onDelete={handleDelete}
-                />
-              ))}
-            </tbody>
-          </table>
-          <div className="px-2 py-4 bg-white border border-gray border-t-transparent">
-            <div className='text-sm text-gray-600'>
-              files: <span>{filesData?.length}</span>
+      {
+        isKnowledgeBaseLoading ? (<div className='flex flex-col items-center justify-center h-[700px]'>
+          <Loader className="border-blue-500 animate-spin" size={50} color="orange" />
+          <p className="text-orange-400">Loading...</p>
+        </div>) : <>
+          {/* header table NTH */}
+          <div className="items-center justify-between gap-6 px-2 py-4 border border-gray-200 md:flex ">
+            <div className='flex gap-2 mb-2 md:mb-0'>
+              <Button
+                color='primary'
+                onClick={() => sortDataByDate(direction)}
+                size="small"
+                variant="outlined"
+                icon={<ArrowDownUp size={14} />}
+              >
+                Sort by date
+              </Button>
+              <Button
+                color='secondary'
+                onClick={() => sortDataByName(direction)}
+                size="small"
+                variant="outlined"
+                icon={<ArrowDownUp size={14} />}
+              >
+                Sort by name
+              </Button>
+            </div>
+            <div >
+              <SearchInput
+                onChange={(e) => console.log(e.target.value)}
+                ref={searchInput}
+              />
             </div>
           </div>
-        </div>
-      </>
+          {/* files */}
+          <div className="mt-2 overflow-x-auto">
+            <table className="min-w-full border divide-y divide-gray-200">
+              <thead className="bg-blue-100">
+                <tr>
+                  <th className="w-1/2 px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase border-b">
+                    Name
+                  </th>
+                  <th className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase border-b">
+                    Date
+                  </th>
+                  <th className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase border-b">
+                    Kind
+                  </th>
+                  <th className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase border-b">
+                    Actions
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {dataValue?.length > 0 && dataValue?.map((file: any, index: any) => (
+                  <FileItem key={`${file.name}-${index}`} item={file}
+                    level={0}
+                    parentId={file.inode_id}
+                    onDelete={handleDelete}
+                  />
+                ))}
+              </tbody>
+            </table>
+            <div className="px-2 py-4 bg-white border border-gray border-t-transparent">
+              <div className='text-sm text-gray-600'>
+                files: <span>{dataValue?.length}</span>
+              </div>
+            </div>
+          </div>
+        </>
+      }
     </div>
   )
 }
